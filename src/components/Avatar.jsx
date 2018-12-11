037027a6b814a93f7630c3e860cbf7abb00f0668
saurabharch <saurabhkashyap0001@gmail.com>
2018-12-11 18:42:12 +0530

    Update Avatar.jsx

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -6,7 +6,7 @@ const Avatar = (props) => (
   <div className={`c-avatar ${props.modifierClasses}`}>
     <img 
       src={(props.modifierClasses === 'c-avatar--as-author') ? logo24 : logo40} 
-      alt="greglobinski.com"
+      alt="saurabhk.com"
     />
   </div>
 )
